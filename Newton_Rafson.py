0c7c7b4378a8a40b57b950d5f52e6363df0cf187
coolhacher_h <swiftkey204@gmail.com>
2023-09-26 22:54:36 +0300

    Методы первого порядка завершены

--- Newton_Rafson.py
+++ Newton_Rafson.py
@@ -1,1 +1,53 @@
-print('hi, world')
+#Метод Ньютона-Рафсона
+#f(x)=exp(0,87x)+exp(0,84x)-144sinx
+import numpy as np
+import math
+
+corsim=4                   #Кол-во верных цифр в 16 системе счисления
+a=0.                         #Интервал поиска минимайзера
+b=np.pi
+eps=0.5*1/(16**(corsim-1))                      #Погрешность
+callf=0
+calldf=0
+callddf=0
+itern=0
+
+def form(x):
+    return str(float.hex(x))[2:10]+' '+str(float.hex(x))[-3:]
+def f(x):
+    global  callf
+    callf+=1
+    return math.exp(0.87*x)+math.exp(0.84*x)-144*math.sin(x)
+
+def df(x):
+    global calldf
+    calldf+=1
+    return 0.87*math.exp(0.87*x)+0.84*math.exp(0.84*x)-144*math.cos(x)
+
+def ddf(x):
+    global callddf
+    callddf += 1
+    return 0.87*0.87 * math.exp(0.87 * x) + 0.84*0.84 * math.exp(0.84 * x) + 144 * math.sin(x)
+
+start=1.
+def new_raf(x):
+    global itern
+    itern+=1
+    df1=df(x)
+    if abs(df1)<eps:
+        return x
+    else:
+        ddf1=ddf(x)
+        print(itern,form(x),float.hex(df1),float.hex(ddf1))
+        return new_raf(x-df1/ddf1)
+
+minimayzer=new_raf(start)
+
+print(f"Минимайзер = {float.hex(minimayzer)}   в 16-ричной")
+s=str(float.hex(minimayzer))
+hans=s[2:3+corsim:]
+
+
+print(f"Функция была вызвана {calldf} раз(а)")
+print(f"Вторая производная функции была вызвана {callddf} раз(а)")
+print(f"Минимайзер = {hans}     16-ричная с {corsim-1} верными цифрами")
--- tg.py
+++ tg.py
@@ -4,14 +4,15 @@ import numpy as np
 import math
 
 corsim=4                   #Кол-во верных цифр в 16 системе счисления
-a=0                         #Интервал поиска минимайзера
+a=0.                         #Интервал поиска минимайзера
 b=np.pi
 eps=0.5*1/(16**(corsim-1))                      #Погрешность
 callf=0
 calldf=0
+itern=0
 
 def form(x):
-    return str(float.hex(x))[2:10]
+    return str(float.hex(x))[2:10]+' '+str(float.hex(x))[-3:]
 def f(x):
     global  callf
     callf+=1
@@ -21,24 +22,32 @@ def df(x):
     global calldf
     calldf+=1
     return 0.87*math.exp(0.87*x)+0.84*math.exp(0.84*x)-144*math.cos(x)
+f1=f(a)
+f2=f(b)
+df1=df(a)
+df2=df(b)
 
-def tg(a,b):
+def tg(a,b,f1,f2,df1,df2):
+    global itern
+    itern+=1
     #print(f"Итерация номер {callf//2+1},  значение: {str(float.hex((b + a) / 2))[2:3 + corsim:]}, с погрешностью {float.hex((b - a) / 2)}")
     if abs(b-a)<eps:
+        print(itern, float.hex(f1), float.hex(f2), float.hex(df1),  float.hex(df2), callf,calldf, sep='   ')
         print(f"Погрешность {float.hex((b-a)/2)}")
         return (b+a)/2
     else:
-        f1,f2,df1,df2=f(a),f(b),df(a),df(b)
         c=(f2-b*df2-f1+a*df1)/(df1-df2)
-        print()
-        if df(c)>=0:
-            return tg(a,c)
+        f3=f(c)
+        df3=df(c)
+        print(itern,form(a),form(c),form(b),float.hex(f1),float.hex(f3),float.hex(f2),float.hex(df1),float.hex(df3),float.hex(df2),float.hex((b-a)/2),callf,calldf,sep='   ')
+        if df3>=0:
+            return tg(a,c,f1,f3,df1,df3)
         else:
-            return tg(c,b)
+            return tg(c,b,f3,f2,df3,df2)
 
-minimayzer=tg(a,b)
+minimayzer=tg(a,b,f1,f2,df1,df2)
 
-print(f"Минимайзер = {float.hex(minimayzer)}   в 10-тичной")
+print(f"Минимайзер = {float.hex(minimayzer)}   в 16-ричной")
 s=str(float.hex(minimayzer))
 hans=s[2:3+corsim:]
 
@@ -46,3 +55,4 @@ hans=s[2:3+corsim:]
 print(f"Функция была вызвана {callf} раз(а)")
 print(f"Производная функции была вызвана {calldf} раз(а)")
 print(f"Минимайзер = {hans}     16-ричная с {corsim-1} верными цифрами")
+print(form(minimayzer))
